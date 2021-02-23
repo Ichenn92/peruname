65854e687625655b29f8001e9800a8cfe4be41ed
Tristan Hörler <tristan.horler@gmail.com>
2021-02-23 11:18:28 +0100

    Add User model configuration and tests on model

--- db/schema.rb
+++ db/schema.rb
@@ -23,14 +23,13 @@ ActiveRecord::Schema.define(version: 2021_02_23_093841) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "actor"
+    t.boolean "actor", default: false, null: false
     t.string "nickname"
     t.string "first_name"
     t.string "last_name"
-    t.string "availibility_start_time"
-    t.string "availibility_end_time"
+    t.time "availibility_start_time"
+    t.time "availibility_end_time"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
