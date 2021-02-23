faf85588387d4a060b9ab634a32926ad7bc5cbae
Clementine Dubuis <clementine.dubuis.mail@gmail.com>
2021-02-23 22:30:18 +0100

    dans le master pas de migration

--- db/schema.rb
+++ db/schema.rb
@@ -35,4 +35,5 @@ ActiveRecord::Schema.define(version: 2021_02_23_150604) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
