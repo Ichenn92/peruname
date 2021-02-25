4901250bccbb65f2059e018cbabb67b47b27a529
Clementine Dubuis <clementine.dubuis.mail@gmail.com>
2021-02-25 21:59:02 +0100

    log in

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   serialize :availibility_start_time, Tod::TimeOfDay
   serialize :availibility_end_time, Tod::TimeOfDay
 
+  attr_accessor :first_name, :last_name
+
   validates_presence_of :nickname, :first_name, :last_name
   validates_inclusion_of :actor, in: [true, false]
 
