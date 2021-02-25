66a7491df8ea75fb355489d5927a763ffbade540
Michael Laurac <michaellaurac@gmail.com>
2021-02-25 19:17:40 +0100

    Added validation of presence of both cith and address

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -6,15 +6,20 @@ class LocationTest < ActiveSupport::TestCase
   # end
   @@base_valid_location = Location.new(city: "Lausanne", address: "Boulevard de Grancy 42")
                                 
-  test "presence of city" do
+  test "reject absence of city" do
     test_location = @@base_valid_location
     test_location.city = ""
     assert_not test_location.valid?, test_location.errors.full_messages.inspect
   end
 
-  test "presence of address" do
+  test "reject absence of address" do
     test_location = @@base_valid_location
     test_location.address = ""
     assert_not test_location.valid?, test_location.errors.full_messages.inspect
+  end
+
+  test "accept presence of city and address" do
+    test_location = @@base_valid_location
+    assert test_location.valid?, test_location.errors.full_messages.inspect
   end
 end
