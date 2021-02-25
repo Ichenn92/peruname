9c196c7703ed2750817b027fdfaecb76a37ac930
Tristan Hörler <tristan.horler@gmail.com>
2021-02-25 21:11:10 +0100

    add character model

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,9 +1,3 @@
 class Character < ApplicationRecord
   belongs_to :character_category
-  enum category: { celebrity: "celebrity",
-                   superhero: "superhero",
-                   anime: "anime",
-                   fake_friend: "fake friend",
-                   other: "other" }
-  validates_presence_of :name, :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_200613) do
+ActiveRecord::Schema.define(version: 2021_02_25_200841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2021_02_25_200613) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name", null: false
+    t.bigint "character_category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["character_category_id"], name: "index_characters_on_character_category_id"
+    t.index ["name"], name: "index_characters_on_name"
   end
 
   create_table "locations", force: :cascade do |t|
@@ -46,4 +55,5 @@ ActiveRecord::Schema.define(version: 2021_02_25_200613) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "characters", "character_categories"
 end
