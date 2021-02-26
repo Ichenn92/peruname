e50f9aa849b9a57868cb5301ffba890b35065eef
Clementine Dubuis <clementine.dubuis.mail@gmail.com>
2021-02-26 10:13:40 +0100

    modify log in and sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  skip_before_action :authenticate_user!, only: :home
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
@@ -9,10 +8,10 @@ class ApplicationController < ActionController::Base
 
  def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :first_name, :last_name])
   end
 
 
