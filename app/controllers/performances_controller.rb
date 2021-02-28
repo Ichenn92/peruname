1c977ebc610b1c16a614dd284e06083ead7dd170
Tristan Hörler <tristan.horler@gmail.com>
2021-02-28 12:10:30 +0100

    add card-performance on search

--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -7,7 +7,7 @@ class PerformancesController < ApplicationController
     #   radius: default 5km
     #   shift(begin_time, end_time)
     #   category
-    authorize @locations = Location.all
+    authorize @locations = Location.where.not(latitude: nil, longitude: nil)
     @performances = @locations.map { |location| location.performances }.flatten
 
     # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,36 +1,36 @@
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-const initMapbox = () => {
-  const mapElement = document.getElementById('map');
+const mapElement = document.getElementById('map');
 
-  if (mapElement) { // only build a map if there's a div#map to inject into
+const addMarkers = (map, markers) => {
+  markers.forEach((marker) => {
+    new mapboxgl.Marker()
+      .setLngLat([marker.lng, marker.lat])
+      .addTo(map);
+  });
+};
+
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+};
+
+const initMapbox = () => {
+  if (mapElement) {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
-  }
-
-  if (mapElement) {
+    
     const markers = JSON.parse(mapElement.dataset.markers);
-    markers.forEach((marker) => {
-      new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
-        .addTo(map);
-    });
+
+    fitMapToMarkers(map, markers);
+    addMarkers(map, markers);
   }
-
-  // const fitMapToMarkers = (map, markers) => {
-  //   const bounds = new mapboxgl.LngLatBounds();
-  //   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  //   map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
-  // };
-  
-  // if (mapElement) {
-  //   fitMapToMarkers(map, markers);
-  // }
-
 };
 
 export { initMapbox };
