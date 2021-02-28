f4b5cb3b8edc0e141771ba6335e6babc960c3b10
Clementine Dubuis <clementine.dubuis.mail@gmail.com>
2021-02-28 21:40:12 +0100

    show card

--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -1,4 +1,6 @@
 class PerformancesController < ApplicationController
+  include Pundit
+  after_action :verify_authorized, except: [:show]
 
   def show
     @performance = Performance.find(params[:id])
