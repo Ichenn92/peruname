8b11dada6392c5cc924e42a4ad236c0d05125035
Tristan Hörler <tristan.horler@gmail.com>
2021-03-01 12:16:25 +0100

    setup form homepage

--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -7,7 +7,7 @@ class PerformancesController < ApplicationController
     #   radius: default 5km
     #   shift(begin_time, end_time)
     #   category
-    authorize @locations = Location.near(params[:location], 5)
+    authorize @locations = Location.near(params[:performance_address], 5)
     @performances = @locations.map { |location| location.performances }.flatten
 
     # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
