1b095e3790d973fac9019c1d9ca1e97883fe3640
Tristan Hörler <tristan.horler@gmail.com>
2021-03-02 14:05:45 +0100

    refactor algorithm for searching

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -1,5 +1,5 @@
 class Performance < ApplicationRecord
-  searchkick
+  reverse_geocoded_by "locations.latitude", "locations.longitude"
 
   belongs_to :user
   belongs_to :character
@@ -13,6 +13,17 @@ class Performance < ApplicationRecord
   validates :location, presence: true
   validates :price_per_hour, presence: true
 
+  def self.search_near_performances_with_filter(params)
+    performances = Performance.joins(:location).near(params[:address], 5)
+
+    if params[:performance][:character_category].present?
+      performances = performances.where(character_id: params[:performance][:character_category])
+    end
+
+    performances
+  end
+
   # TODO: add validation of user where actor is true
   # TODO: add validation that price_per_hour is an integer > 10
+
 end
