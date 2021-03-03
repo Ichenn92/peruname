e7411be1f98cfe7fb3535822f484a9ad57c02ef5
Tristan Hörler <tristan.horler@gmail.com>
2021-03-03 23:02:07 +0100

    add settings to navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :first_name, :last_name, :photo])
   end
 
   # Uncomment when you *really understand* Pundit!
