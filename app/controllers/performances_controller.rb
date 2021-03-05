c198ba1f7cce18bcc8d3a2e03ab1a79493902ace
Tristan Hörler <tristan.horler@gmail.com>
2021-03-05 02:18:25 +0100

    fix performance controller fallback issue

--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -27,30 +27,31 @@ class PerformancesController < ApplicationController
       @character_category = CharacterCategory.find(performance_params[:character_category].to_i) unless performance_params[:character_category].blank?
       @performance_category = PerformanceCategory.find(performance_params[:performance_category_id].to_i) unless performance_params[:performance_category_id].blank?
       @description = performance_params[:description] unless performance_params[:description].blank?
-      @location = Location.find_by_name(performance_params[:location].split(' / ').first) unless performance_params[:location].blank?
+      @location = Location.find_by_name(performance_params[:location].split(" / ").first) unless performance_params[:location].blank?
       @price_per_hour = performance_params[:price_per_hour] unless performance_params[:price_per_hour].blank?
 
       authorize @performance = Performance.new({ description: @description,
-                                                  price_per_hour: @price_per_hour,
-                                                  character: @character,
-                                                  character_category: @character_category,
-                                                  performance_category: @performance_category,
-                                                  location: @location,
-                                                  user: current_user })
+                                                 price_per_hour: @price_per_hour,
+                                                 character: @character,
+                                                 character_category: @character_category,
+                                                 performance_category: @performance_category,
+                                                 location: @location,
+                                                 user: current_user })
       current_user.actor = current_user.performances.count > 0
       if @performance.save
         redirect_to performance_path(@performance)
       else
         render :new
-      end 
+      end
     end
   end
 
   private
 
   def no_result_found
-    flash.now[:alert] = "No result found"
-    render "pages/home"
+    flash[:alert] = "No result found with those parameters"
+    redirect_back fallback_location: root_path
+    # render "pages/home"
   end
 
   def set_markers # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
