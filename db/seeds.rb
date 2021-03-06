a74c6b5ec4c0e40bfe78f640d697d04ea6181520
Michael Laurac <michaellaurac@gmail.com>
2021-03-06 13:03:42 +0100

    Stuffed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,7 +24,42 @@ montriond = { name: "Le Montriond", address: "Avenue Édouard Dapples 25, 1006 L
 petit_theatre = { name: "Le Petit Théâtre", address: "Place de la Cathédrale 12, 1005 Lausanne" }
 terrasse_des_grandes_roches = { name: "Terrasse des Grandes Roches", address: "Escaliers des Grandes-Roches, 1003 Lausanne" }
 freddy_mercury_statue = { name: "Freddie Mercury Statue", address: "Place du Marché, 1820 Montreux" }
-[zoom, slack, skype, caffe_spettacolo, cafe_de_la_plage, etoile, montriond, petit_theatre, terrasse_des_grandes_roches, freddy_mercury_statue].each do |attribute|
+theatre_des_lutins = { name: "Théâtre des Lutins", address: "Rue du Petit-Saint-Jean 1A, 1003 Lausanne" }
+theatre_du_vide_poche = { name: "Théâtre du Vide-Poche", address: "Place de la Palud 10, 1003 Lausanne" }
+restaurant_du_theatre = { name: "Restaurant du Théâtre", address: "Avenue du Théâtre 12, 1005 Lausanne" }
+theatre_2_21 = { name: "Théâtre 2.21", address: "Rue de l'Industrie 10, 1005 Lausanne" }
+cafe_restaurant_le_valentin = { name: "Café Restaurant Le Valentin", address: "Rue du Valentin 30, 1004 Lausanne" }
+culture_cafe = { nane: "Culture Café", address: "Rue de Genève 6, 1003 Lausanne" }
+cafe_des_artisans = { name: "Café des Artisans", address: "Rue Centrale 16, 1003 Lausanne" }
+the_coffee_project = { name: "The Coffee Project", address: "Rue Marterey 1-3, 1005 Lausanne" }
+cafe_la_couronne_d_or = { name: "Café La Couronne d'Or", address: "Rue des Deux-Marchés 13, 1005 Lausanne" }
+coccinelle_cafe = { name: "Coccinelle Café", address: "Rue Pichard 18, 1003 Lausanne" }
+cafe_restaurant_mozart = { name: "Café Restaurant Mozart", address: "Rue de la Grotte 2, 1003 Lausanne" }
+cafe_des_avenues = { name: "Café des Avenues", address: "Avenue de Jurigoz, Avenue de Montchoisi 20, 1006 Lausanne" }
+cafe_de_grancy = { name: "Café de Grancy", address: "Avenue du Rond-Point 1, 1006 Lausanne" }
+
+[ zoom, 
+  slack, 
+  skype, 
+  caffe_spettacolo, 
+  cafe_de_la_plage, 
+  etoile, 
+  montriond, 
+  petit_theatre, 
+  terrasse_des_grandes_roches, 
+  freddy_mercury_statue, 
+  theatre_des_lutins, 
+  theatre_du_vide_poche,
+  restaurant_du_theatre,
+  theatre_2_21,
+  cafe_restaurant_le_valentin,
+  culture_cafe,
+  cafe_des_artisans,
+  the_coffee_project,
+  cafe_la_couronne_d_or,
+  coccinelle_cafe,
+  cafe_restaurant_mozart,
+  cafe_des_avenues ].each do |attribute|
   location = Location.create!(attribute)
   puts "Setting a marker for #{location.name}"
 end
@@ -35,15 +70,23 @@ fake_friend = { name: "Fake friend" }
 profession = { name: "Profession" }
 celebrity = { name: "Celebrity" }
 superhero = { name: "Superhero" }
+fiction = { name: "Fiction" }
+old_fashioned = { name: "Old fashioned"}
 others = { name: "Others" }
-[anime, fake_friend, profession, celebrity, superhero, others].each do |attribute|
+[ anime, 
+  fake_friend, 
+  profession, 
+  celebrity, 
+  superhero, 
+  fiction, 
+  old_fashioned, 
+  others ].each do |attribute|
   character_category = CharacterCategory.create!(attribute)
   puts "Adding a new character category for #{character_category.name}"
 end
 
 puts "Imagining some characters..."
 pikachu = { name: "Pikachu", character_category: CharacterCategory.find_by_name("Anime") }
-puts pikachu
 totoro = { name: "Totoro", character_category: CharacterCategory.find_by_name("Anime") }
 candy = { name: "Candy", character_category: CharacterCategory.find_by_name("Anime") }
 jiren = { name: "Jiren", character_category: CharacterCategory.find_by_name("Anime") }
@@ -53,19 +96,83 @@ chuck_norris = { name: "Chuck Norris", character_category: CharacterCategory.fin
 elon_musk = { name: "Elon Musk", character_category: CharacterCategory.find_by_name("Celebrity") }
 beyonce = { name: "Beyonce", character_category: CharacterCategory.find_by_name("Celebrity") }
 greta_thunberg = { name: "Greta Thunberg", character_category: CharacterCategory.find_by_name("Celebrity") }
+marylin_monroe = { name: "Marylin Monroe", character_category: CharacterCategory.find_by_name("Celebrity") }
+marylin_manson = { name: "Marylin Manson", character_category: CharacterCategory.find_by_name("Celebrity") }
 iron_man = { name: "Iron Man", character_category: CharacterCategory.find_by_name("Superhero") }
 thor = { name: "Thor", character_category: CharacterCategory.find_by_name("Superhero") }
 wonder_woman = { name: "Wonder Woman", character_category: CharacterCategory.find_by_name("Superhero") }
 batman = { name: "Batman", character_category: CharacterCategory.find_by_name("Superhero") }
+hulk = { name: "Hulk", character_category: CharacterCategory.find_by_name("Superhero") }
+captain_america = { name: "Captain America", character_category: CharacterCategory.find_by_name("Superhero") }
 teacher = { name: "Teacher", character_category: CharacterCategory.find_by_name("Profession") }
 car_repairman = { name: "Car repairman", character_category: CharacterCategory.find_by_name("Profession") }
 taxidermist = { name: "Taxidermist", character_category: CharacterCategory.find_by_name("Profession") }
-web_developer = { name: "Web Developer", character_category: CharacterCategory.find_by_name("Profession") }
+web_developer = { name: "Web developer", character_category: CharacterCategory.find_by_name("Profession") }
+president_of_the_united_states = { name: "President of the United States", character_category: CharacterCategory.find_by_name("Profession") }
+reporter = { name: "Reporter", character_category: CharacterCategory.find_by_name("Profession") }
+pediatric_heart_surgeon = { name: "Pediatric heart surgeon", character_category: CharacterCategory.find_by_name("Profession") }
+chief_executive_officer = { name: "Chief executive officer", character_category: CharacterCategory.find_by_name("Profession") }
 brandon = { name: "Brandon", character_category: CharacterCategory.find_by_name("Fake friend") }
 julia = { name: "Julia", character_category: CharacterCategory.find_by_name("Fake friend") }
 michel = { name: "Michel", character_category: CharacterCategory.find_by_name("Fake friend") }
 blandine = { name: "Blandine", character_category: CharacterCategory.find_by_name("Fake friend") }
-[pikachu, totoro, candy, jiren, soeur_emmanuelle, donald_trump, chuck_norris, elon_musk, beyonce, greta_thunberg, iron_man, wonder_woman, batman, teacher, car_repairman, taxidermist, web_developer, brandon, julia, michel, blandine].each do |attribute|
+thomas = { name: "Thomas", character_category: CharacterCategory.find_by_name("Fake friend") }
+emma = { name: "Emma", character_category: CharacterCategory.find_by_name("Fake friend") }
+dr_who = { name: "Dr Who", character_category: CharacterCategory.find_by_name("Fiction") }
+darth_vador = { name: "Darth Vador", character_category: CharacterCategory.find_by_name("Fiction") }
+harry_potter = { name: "Harry Potter", character_category: CharacterCategory.find_by_name("Fiction") }
+the_wizard_of_oz = { name: "The Wizard of Oz", character_category: CharacterCategory.find_by_name("Fiction") }
+jon_snow = { name: "Jon Snow", character_category: CharacterCategory.find_by_name("Fiction") }
+louis_xiv = { name: "Louis XIV", character_category: CharacterCategory.find_by_name("Old fashioned") }
+staline = { name: "Staline", character_category: CharacterCategory.find_by_name("Old fashioned") }
+abraham_lincoln = { name: "Abraham Lincoln", character_category: CharacterCategory.find_by_name("Old fashioned") }
+mahatma_gandhi = { name: "Mahatma Gandhi", character_category: CharacterCategory.find_by_name("Old fashioned") }
+d_artagnan = { name: "D'Artagnan", character_category: CharacterCategory.find_by_name("Old fashioned") }
+napoleon = { name: "Napoleon", character_category: CharacterCategory.find_by_name("Old fashioned") }
+william_tell = { name: "William Tell", character_category: CharacterCategory.find_by_name("Old fashioned") }
+[ pikachu, 
+  totoro, 
+  candy, 
+  jiren, 
+  soeur_emmanuelle, 
+  donald_trump, 
+  chuck_norris, 
+  elon_musk, 
+  beyonce, 
+  greta_thunberg,
+  marylin_monroe, 
+  marylin_manson,
+  iron_man, 
+  wonder_woman, 
+  batman,
+  hulk,
+  captain_america,
+  teacher, 
+  car_repairman, 
+  taxidermist, 
+  web_developer,
+  president_of_the_united_states,
+  reporter,
+  pediatric_heart_surgeon,
+  ceo,
+  brandon, 
+  julia, 
+  michel, 
+  blandine,
+  thomas,
+  emma,
+  dr_who,
+  darth_vador,
+  harry_potter,
+  the_wizard_of_oz,
+  jon_snow,
+  louis_xiv,
+  staline,
+  abraham_lincoln,
+  mahatma_gandhi,
+  d_artagnan,
+  napoleon,
+  william_tell ].each do |attribute|
   character = Character.create!(attribute)
   puts "Spawning a new character called #{character.name}"
 end
@@ -101,9 +208,9 @@ photos = [
     content_type: "image/png",
   },
   {
-    io: URI.open("https://kitt.lewagon.com/placeholder/users/clem13-coder"),
-    filename: "eytan.png",
-    content_type: "image/png",
+    io: URI.open("https://res.cloudinary.com/clemdub1324/image/upload/v1615029891/pexels-emre-keshavarz-5191436_tmq0nl.jpg"),
+    filename: "eytan.jpg",
+    content_type: "image/jpg",
   },
   {
     io: URI.open("https://upload.wikimedia.org/wikipedia/commons/6/62/Thomas_Wiesel.png"),
@@ -174,7 +281,7 @@ performance_8 = { description: "This is a rare performance which you will never 
                   user: User.find_by_nickname("El Capo"),
                   performance_category: PerformanceCategory.find_by_name("Romantic"),
                   location: Location.find_by_name("Caffè Spettacolo") }
-performance_9 = { description: "If you're fund of animal chasing and mangas, you know how to catch them all, except this is only chance to catch this one during a special date.",
+performance_9 = { description: "If you're fond of animal chasing and mangas, you know how to catch them all, except this is only chance to catch this one during a special date.",
                   price_per_hour: 49,
                   character: Character.find_by_name("Pikachu"),
                   user: User.find_by_nickname("Alibaba"),
@@ -185,7 +292,37 @@ performance_10 = { description: "All carried by her engagement and enthusiam of 
                    character: Character.find_by_name("Greta Thunberg"),
                    user: User.find_by_nickname("Gibella"),
                    performance_category: PerformanceCategory.find_by_name("Depressive"),
-                   location: Location.find_by_name("Freddie Mercury Statue") }
+                  location: Location.find_by_name("Freddie Mercury Statue") }
+performance_11 = { description: "No one can better represent the pop culture than a Marylin Monroe, who wants to be loved by you.",
+                   price_per_hour: 42,
+                   character: Character.find_by_name("Marylin Monroe"),
+                   user: User.find_by_nickname("Gibella"),
+                   performance_category: PerformanceCategory.find_by_name("Romantic"),
+                   location: Location.find_by_name("Café des Avenues") }
+performance_12 = { description: "Beware the dark side of the force, as it might lead to intricated family stories",
+                   price_per_hour: 33,
+                   character: Character.find_by_name("Darth Vador"),
+                   user: User.find_by_nickname("El Capo"),
+                   performance_category: PerformanceCategory.find_by_name("Parody"),
+                   location: Location.find_by_name("Café de Grancy") }
+performance_13 = { description: "Have you ever dreamt of travelling space and time with an unsual time lord with two hearts?",
+                   price_per_hour: 28,
+                   character: Character.find_by_name("Dr Who"),
+                   user: User.find_by_nickname("El Capo"),
+                   performance_category: PerformanceCategory.find_by_name("Funny"),
+                   location: Location.find_by_name("Théâtre 2.21") }
+performance_14 = { description: "A hand on your chest, you will have the chance to revive the greatest emperor of the European history, after Julius",
+                   price_per_hour: 24,
+                   character: Character.find_by_name("Napoleon"),
+                   user: User.find_by_nickname("Alibaba"),
+                   performance_category: PerformanceCategory.find_by_name("Depressive"),
+                   location: Location.find_by_name("Théâtre des Lutins") }
+performance_15 = { description: "Your favorite teenager wizard, with his broomstick and glasses",
+                   price_per_hour: 24,
+                   character: Character.find_by_name("Harry Potter"),
+                   user: User.find_by_nickname("Alibaba"),
+                   performance_category: PerformanceCategory.find_by_name("Realistic"),
+                   location: Location.find_by_name("Café de la Couronne d'Or") }
 photos = [
   {
     io: URI.open("https://images.unsplash.com/photo-1592525969064-afa927e72ccf?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"),
@@ -237,9 +374,49 @@ photos = [
     filename: "performance_10.png",
     content_type: "image/png",
   },
+  {
+    io: URI.open("https://images.unsplash.com/photo-1611699066085-ba118c694596?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80"),
+    filename: "performance_11.png",
+    content_type: "image/png",
+  },
+  {
+    io: URI.open("https://pbs.twimg.com/profile_images/1346042465917218820/Dc2ZGKb8.jpg"),
+    filename: "performance_12.jpg",
+    content_type: "image/jpg",
+  },
+  {
+    io: URI.open("https://pbs.twimg.com/profile_images/1346042465917218820/Dc2ZGKb8.jpg"),
+    filename: "performance_13.jpg",
+    content_type: "image/jpg",
+  },
+  {
+    io: URI.open("https://cdn.pixabay.com/photo/2016/11/11/12/01/tardis-1816598_1280.jpg"),
+    filename: "performance_14.jpg",
+    content_type: "image/jpg",
+  },
+  {
+    io: URI.open("https://unsplash.com/photos/3n7DdlkMfEg"),
+    filename: "performance_15.jpg",
+    content_type: "image/jpg",
+  },
 ]
 
-[performance_1, performance_2, performance_3, performance_4, performance_5, performance_6, performance_7, performance_8, performance_9, performance_10].each_with_index do |attribute, index|
+[ performance_1, 
+  performance_2, 
+  performance_3, 
+  performance_4, 
+  performance_5, 
+  performance_6, 
+  performance_7, 
+  performance_8, 
+  performance_9, 
+  performance_10,
+  performance_11,
+  performance_12,
+  performance_13,
+  performance_14,
+  performance_15,
+].each_with_index do |attribute, index|
   performance = Performance.create!(attribute)
 
   performance.photos.attach(io: photos[index][:io], filename: photos[index][:filename], content_type: photos[index][:content_type])
@@ -253,7 +430,7 @@ until Booking.count > 30
     start_time: Tod::TimeOfDay.new(rand(8..12)),
     end_time: Tod::TimeOfDay.new(rand(13..17)),
     client_id: rand(1..5),
-    performance: Performance.find(rand(1..10)),
+    performance: Performance.find(rand(1..15)),
     status: 0,
   )
   booking.actor_id = booking.performance.user.id
